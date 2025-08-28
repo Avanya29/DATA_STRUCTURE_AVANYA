7535de7b409d148548abbf684c842b648df99f12
AVANYA <avanyasangal29@gmail.com>
2025-08-28 13:43:23 +0530

    data structure first day

---
+++ duplication.cpp
@@ -0,0 +1,20 @@
+
+#include <algorithm>
+#include<iostream>
+
+
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+//input of array
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+ int size= max(arr.length,n);
+
+
+}
